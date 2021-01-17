f92f240d8de36ca00f066d5cbcc11e23857ec68b
Tim Yashin <t.yashin@tuta.io>
2021-01-17 05:34:22 +0300

    Project structure setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      color="primary"
+      dark
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>UOL own policies violations tracker</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({ drawer: null })
+}
+</script>
---
+++ how_to.md
@@ -0,0 +1,5 @@
+# Editing process
+
+- Fork this repo
+- Edit file README.MD inside the root directory
+- Create pull request
